4b66f2e82bdb2f8aec6a5b3646dd4b12cadd95fb
csesumonpro <csesumonpro@gmail.com>
2023-06-13 21:29:12 +0600

    init

---
+++ wp-todo-plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Plugin Name:       Wp Todo Plugin
+ * Plugin URI:        https://wordpress.org/plugins/wp-todo-plugin/
+ * Description:       A plugin to manage your todo list
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            CleanCodeWithSumon
+ * Author URI:        https://CleanCodeWithSumon.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-todo-plugin
+ * Domain Path:       /languages
+ */
