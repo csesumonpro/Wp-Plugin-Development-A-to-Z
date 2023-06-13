8da87b08f8c42f9a1ffc19b198ed4b3b78c788eb
csesumonpro <csesumonpro@gmail.com>
2023-06-13 22:59:14 +0600

    Added all about plugin basics

--- wp-todo-plugin.php
+++ wp-todo-plugin.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'you are not allowed to access this page directly' );
+}
+
 /*
  * Plugin Name:       Wp Todo Plugin
  * Plugin URI:        https://wordpress.org/plugins/wp-todo-plugin/
@@ -14,3 +18,36 @@
  * Text Domain:       wp-todo-plugin
  * Domain Path:       /languages
  */
+
+register_activation_hook(
+	__FILE__,
+	'wp_todo_plugin_register_activation'
+);
+
+function wp_todo_plugin_register_activation()
+{
+//	var_dump( 'activation' );
+//	exit();
+}
+
+register_deactivation_hook(
+	__FILE__,
+	'wp_todo_plugin_register_deactivation'
+);
+
+function wp_todo_plugin_register_deactivation()
+{
+//	var_dump( 'deactivation' );
+//	exit();
+}
+
+register_uninstall_hook(
+	__FILE__,
+	'wp_todo_plugin_register_uninstall'
+);
+
+function wp_todo_plugin_register_uninstall()
+{
+//	var_dump( 'uninstall' );
+//	exit();
+}
