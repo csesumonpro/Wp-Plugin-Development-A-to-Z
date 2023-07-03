b667fc4be33b33a1c7445ffab2fc3580d19fc39d
csesumonpro <csesumonpro@gmail.com>
2023-07-03 22:03:33 +0600

    Added cusotm post type

--- wp-todo-plugin.php
+++ wp-todo-plugin.php
@@ -19,57 +19,81 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Domain Path:       /languages
  */
 
-class WP_Todo_Plugin_Meta_Box {
+// Register Custom Post Type Wp Todo
+function create_wptodo_cpt() {
 
-	/**
-	 * Set up and add the meta box.
-	 */
-	public static function add() {
-		$screens = [ 'post', 'page' ];
-		foreach ( $screens as $screen ) {
-			add_meta_box(
-				'wporg_box_id',          // Unique ID
-				'Custom Meta Box Title', // Box title
-				[ self::class, 'html' ],   // Content callback, must be of type callable
-				$screen                  // Post type
-			);
-		}
-	}
+	$labels = array(
+		'name' => _x( 'Wp Todos', 'Post Type General Name', 'wp-todo-plugin' ),
+		'singular_name' => _x( 'Wp Todo', 'Post Type Singular Name', 'wp-todo-plugin' ),
+		'menu_name' => _x( 'Wp Todos', 'Admin Menu text', 'wp-todo-plugin' ),
+		'name_admin_bar' => _x( 'Wp Todo', 'Add New on Toolbar', 'wp-todo-plugin' ),
+		'archives' => __( 'Wp Todo Archives', 'wp-todo-plugin' ),
+		'attributes' => __( 'Wp Todo Attributes', 'wp-todo-plugin' ),
+		'parent_item_colon' => __( 'Parent Wp Todo:', 'wp-todo-plugin' ),
+		'all_items' => __( 'All Wp Todos', 'wp-todo-plugin' ),
+		'add_new_item' => __( 'Add New Wp Todo', 'wp-todo-plugin' ),
+		'add_new' => __( 'Add New', 'wp-todo-plugin' ),
+		'new_item' => __( 'New Wp Todo', 'wp-todo-plugin' ),
+		'edit_item' => __( 'Edit Wp Todo', 'wp-todo-plugin' ),
+		'update_item' => __( 'Update Wp Todo', 'wp-todo-plugin' ),
+		'view_item' => __( 'View Wp Todo', 'wp-todo-plugin' ),
+		'view_items' => __( 'View Wp Todos', 'wp-todo-plugin' ),
+		'search_items' => __( 'Search Wp Todo', 'wp-todo-plugin' ),
+		'not_found' => __( 'Not found', 'wp-todo-plugin' ),
+		'not_found_in_trash' => __( 'Not found in Trash', 'wp-todo-plugin' ),
+		'featured_image' => __( 'Featured Image', 'wp-todo-plugin' ),
+		'set_featured_image' => __( 'Set featured image', 'wp-todo-plugin' ),
+		'remove_featured_image' => __( 'Remove featured image', 'wp-todo-plugin' ),
+		'use_featured_image' => __( 'Use as featured image', 'wp-todo-plugin' ),
+		'insert_into_item' => __( 'Insert into Wp Todo', 'wp-todo-plugin' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Wp Todo', 'wp-todo-plugin' ),
+		'items_list' => __( 'Wp Todos list', 'wp-todo-plugin' ),
+		'items_list_navigation' => __( 'Wp Todos list navigation', 'wp-todo-plugin' ),
+		'filter_items_list' => __( 'Filter Wp Todos list', 'wp-todo-plugin' ),
+	);
+	$args = array(
+		'label' => __( 'Wp Todo', 'wp-todo-plugin' ),
+		'description' => __( '', 'wp-todo-plugin' ),
+		'labels' => $labels,
+		'menu_icon' => 'dashicons-list-view',
+		'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'author', 'comments', 'page-attributes', 'post-formats', 'custom-fields'),
+		'taxonomies' => ['category', 'post_tag'],
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'can_export' => true,
+		'has_archive' => true,
+		'hierarchical' => false,
+		'exclude_from_search' => false,
+		'show_in_rest' => true,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
+	);
+	register_post_type( 'wp-todo-plugin', $args );
+
+}
+add_action( 'init', 'create_wptodo_cpt', 0 );
 
 
-	/**
-	 * Save the meta box selections.
-	 *
-	 * @param int $post_id  The post ID.
-	 */
-	public static function save( int $post_id ) {
-		if ( array_key_exists( 'wp_todo_plugin_field', $_POST ) ) {
-			update_post_meta(
-				$post_id,
-				'wp_todo_plugin_field',
-				$_POST['wp_todo_plugin_field']
-			);
-		}
-	}
+add_action('init', function () {
+	$args = array(
+		'post_type'      => 'wp-todo-plugin',
+		'posts_per_page' => 10,
+	);
 
-
-	/**
-	 * Display the meta box HTML to the user.
-	 *
-	 * @param WP_Post $post   Post object.
-	 */
-	public static function html( $post ) {
-		$value = get_post_meta( $post->ID, 'wp_todo_plugin_field', true );
+	$loop = new WP_Query($args);
+	while ( $loop->have_posts() ) {
+		$loop->the_post();
 		?>
-        <label for="wporg_field">Description for this field</label>
-        <select name="wp_todo_plugin_field" id="wporg_field" class="postbox">
-            <option value="">Select something...</option>
-            <option value="something" <?php selected( $value, 'something' ); ?>>Something</option>
-            <option value="else" <?php selected( $value, 'else' ); ?>>Else</option>
-        </select>
+		<div class="entry-content">
+			<?php the_title(); ?>
+			<?php the_content(); ?>
+		</div>
 		<?php
 	}
-}
 
-add_action( 'add_meta_boxes', [ 'WP_Todo_Plugin_Meta_Box', 'add' ]);
-add_action( 'save_post', [ 'WP_Todo_Plugin_Meta_Box', 'save' ] );
+	exit();
+});
